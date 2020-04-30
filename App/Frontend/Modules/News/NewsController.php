eb70181377c09f3efeb7e5a49c465c7395a33fe3
d3b14n21 <gnicolas21@hotmail.com>
2020-04-30 13:00:39 +0200

    Execute Index done

--- App/Frontend/Modules/News/NewsController.php
+++ App/Frontend/Modules/News/NewsController.php
@@ -1,7 +1,33 @@
 <?php
-namespace App\FrontEnd\Modules\News;
 
-class NewsController
+namespace App\Frontend\Modules\News;
+
+use \OCFram\BackController;
+use \OCFram\HTTPRequest;
+
+class NewsController extends BackController
 {
+    public function executeIndex(HTTPRequest $request)
+    {
+        $nombreNews = $this->app->config()->get('nombre_news');
+        $nombreCaracteres = $this->app->config()->get('nombre_caracteres');
 
+        $this->page->addVar('title', 'Liste des ' . $nombreNews . ' dernières news');
+
+        $manager = $this->managers->getManagerOf('News');
+
+        $listeNews = $manager->getList(0, $nombreNews);
+
+        foreach ($listeNews as $news) {
+            if (strlen($news->contenu()) > $nombreCaracteres) {
+                $debut = substr($news->contenu(), 0, $nombreCaracteres);
+                $debut = substr($debut, 0, strrpos($debut, ' ')) . '...';
+
+                $news->setContenu($debut);
+            }
+        }
+
+        // On ajoute la variable $listeNews à la vue.
+        $this->page->addVar('listeNews', $listeNews);
+    }
 }
--- lib/vendors/Model/NewsManager.php
+++ lib/vendors/Model/NewsManager.php
@@ -1,7 +1,16 @@
 <?php
 
+namespace Model;
 
-class NewsManager
+use \OCFram\Manager;
+
+abstract class NewsManager extends Manager
 {
-
+    /**
+     * Méthode retournant une liste de news demandée
+     * @param $debut int La première news à sélectionner
+     * @param $limite int Le nombre de news à sélectionner
+     * @return array La liste des news. Chaque entrée est une instance de News.
+     */
+    abstract public function getList($debut = -1, $limite = -1);
 }
