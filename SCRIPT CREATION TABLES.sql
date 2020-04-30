6da92a842b362d2f5aac98ab090741ee3ac2b594
d3b14n21 <gnicolas21@hotmail.com>
2020-04-30 15:08:38 +0200

    rectification

--- SCRIPT CREATION TABLES.sql
+++ SCRIPT CREATION TABLES.sql
@@ -7,3 +7,12 @@ CREATE TABLE IF NOT EXISTS `news` (
   `dateModif` datetime NOT NULL,
   PRIMARY KEY (`id`)
 ) DEFAULT CHARSET=utf8 ;
+
+CREATE TABLE IF NOT EXISTS `comments2` (
+  `id` mediumint(9) NOT NULL AUTO_INCREMENT,
+  `news` smallint(6) NOT NULL,
+  `auteur` varchar(50) NOT NULL,
+  `contenu` text NOT NULL,
+  `date` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) DEFAULT CHARSET=utf8 ;
